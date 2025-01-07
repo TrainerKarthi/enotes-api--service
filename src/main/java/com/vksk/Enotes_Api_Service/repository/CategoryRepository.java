243ac722e0ebca47a0edf43a8250ac3df9321af6
Karthik2502 <vkarthik2502@gmail.com>
2025-01-07 14:50:49 +0530

    dto mapper implementation in category

--- src/main/java/com/vksk/Enotes_Api_Service/repository/CategoryRepository.java
+++ src/main/java/com/vksk/Enotes_Api_Service/repository/CategoryRepository.java
@@ -1,9 +1,13 @@
 package com.vksk.Enotes_Api_Service.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.vksk.Enotes_Api_Service.entity.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+	List<Category> findByIsActiveTrue();
 
 }
--- src/main/java/com/vksk/Enotes_Api_Service/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/vksk/Enotes_Api_Service/service/impl/CategoryServiceImpl.java
@@ -1,12 +1,16 @@
 package com.vksk.Enotes_Api_Service.service.impl;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
+import com.vksk.Enotes_Api_Service.dto.CategoryDto;
+import com.vksk.Enotes_Api_Service.dto.CategoryResponse;
 import com.vksk.Enotes_Api_Service.entity.Category;
 import com.vksk.Enotes_Api_Service.repository.CategoryRepository;
 import com.vksk.Enotes_Api_Service.service.CategoryService;
@@ -17,22 +21,47 @@ public class CategoryServiceImpl implements CategoryService {
 	@Autowired
 	private CategoryRepository repo;
 
+	@Autowired
+	private ModelMapper mapper;
+
 	@Override
-	public boolean saveCategory(Category category) {
+	public boolean saveCategory(CategoryDto categoryDto) {
+
+//		Category category = new Category();
+//		category.setName(categoryDto.getName());
+//		category.setDescription(categoryDto.getDescription());
+//		category.setIsActive(categoryDto.getIsActive());
+
+		Category category = mapper.map(categoryDto, Category.class);
+
 		category.setIsDeleted(false);
 		category.setCreatedBy(1);
 		category.setCreatedOn(new Date());
+
 		Category category2 = repo.save(category);
 
 		return !ObjectUtils.isEmpty(category2);
 	}
 
 	@Override
-	public List<Category> findAllCategories() {
+	public List<CategoryDto> findAllCategories() {
 
 		List<Category> allCategories = repo.findAll();
 
-		return allCategories;
+		List<CategoryDto> list = new ArrayList<CategoryDto>();
+		for (Category category : allCategories) {
+			list.add(mapper.map(category, CategoryDto.class));
+		}
+
+		return list;
+	}
+
+	@Override
+	public List<CategoryResponse> findByActiveCategory() {
+		List<Category> list = repo.findByIsActiveTrue();
+
+		List<CategoryResponse> cResponses = list.stream().map(cat -> mapper.map(cat, CategoryResponse.class)).toList();
+		return cResponses;
 	}
 
 }
