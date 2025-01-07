9e8e21492cd211622241efd51a026efa2cab7a3a
Karthik2502 <vkarthik2502@gmail.com>
2025-01-07 13:22:57 +0530

    save get category implement

---
+++ src/main/java/com/vksk/Enotes_Api_Service/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.vksk.Enotes_Api_Service.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.vksk.Enotes_Api_Service.entity.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+}
